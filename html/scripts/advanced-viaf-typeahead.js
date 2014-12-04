0424640eb814e254ad0b93ca5ea683ffb723ea5e
Jason Thomale <jason@Zepp.domain_not_set.invalid>
2014-12-04 00:27:22 -0500

    A little restructuring of the front-end components...

---
+++ html/scripts/advanced-viaf-typeahead.js
@@ -0,0 +1,106 @@
+/*
+advanced-viaf-typeahead.js
+
+jQuery plugin that adds an "advanced" VIAF typeahead to a text input
+form field, which groups returned VIAF headings by entity type.
+
+Requirements:
+
+typeahead.js
+
+
+*/
+
+(function($) {
+  /*entityTypes maps from the nametype contained in the VIAF response to
+  a more readable entity type.
+  */
+  var entityTypes = {
+    personal: 'People',
+    corporate: 'Organizations',
+    geographic: 'Places',
+    subject: 'Topics',
+    uniformtitlework: 'Works',
+    uniformtitleexpression: 'Expressions',
+  }
+
+  /*
+  filterResponse is our function that filters the response from VIAF
+  and puts it into a data structure to be sent to the typeahead template.
+  In this case it groups response results by entity type before adding
+  them to the return array that feeds the typeahead dropdown.
+  */
+  var filterResponse = function(response) {
+    var ret_array = [];
+    var entities = {};
+    // we only return anything if we got something in the response
+    if (response.result !== null) {
+      // first loop through the response and group terms by entities
+      $.each(response.result, function(i, result) {
+        var nameType = entityTypes[result.nametype];
+        var term = result.term;
+        if (entities[nameType] !== 'undefined') {
+          if (!entities[nameType]) {
+            entities[nameType] = [];
+          }
+          // basic deduping
+          if ($.inArray(term, entities[nameType]) == -1) {
+            entities[nameType].push(term);
+          }
+        }
+      });
+      // now append grouped terms to the return array
+      for (key in entities) {
+        $.each(entities[key], function(i, term) {
+          // make sure we hide the entity type header if this isn't the first
+          // result in the group
+          var hidden = i == 0 ? '' : 'hidden';
+          ret_array.push({value: term, nameType: key, hideHeader: hidden});
+        });
+      }
+    }
+    return ret_array;
+  }
+
+  $.fn.advancedViafTypeahead = function(options) {
+    var settings = $.extend({
+      /* max limit is 10 items at a time because that's all we can get
+      from VIAF */
+      limit: 10,
+      hint: true,
+      highlight: false,
+      minLength: 3,
+      name: 'entities',
+      templates: {}
+    }, options);
+
+    /*suggestEngine is our Bloodhound typeahead suggestion object, which
+    serves as the typeahead data source*/
+    var suggestEngine = new Bloodhound({
+      name: settings.name,
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      limit: settings.limit,
+      remote: {
+        ajax: {dataType: 'jsonp'},
+        url: 'http://viaf.org/viaf/AutoSuggest?query=%QUERY',
+        filter: filterResponse
+      }
+    });
+
+    suggestEngine.initialize();    
+
+    this.typeahead({
+      hint: settings.hint,
+      minLength: settings.minLength
+    },
+    {
+      name: settings.name,
+      displayKey: 'value',
+      source: suggestEngine.ttAdapter(),
+      templates: settings.templates
+    });
+
+    return this;
+  }
+}(jQuery));
--- html/scripts/main.js
+++ html/scripts/main.js
@@ -1,79 +1,73 @@
-/*entityTypes maps from the nametype contained in the VIAF response to
-a more presentable entity type.
-*/
-var entityTypes = {
-  personal: 'People',
-  corporate: 'Organizations',
-  geographic: 'Places',
-  subject: 'Topics',
-  uniformtitlework: 'Works',
-  uniformtitleexpression: 'Expressions',
+/*Utility Functions*/
+
+var getUrlParam = function(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
 
-/*
-filterResponse is our function that filters the response from VIAF
-and puts it into a data structure to be sent to the typeahead template.
-In this case it groups response results by entity type before adding
-them to the return array that feeds the typeahead dropdown.
-*/
-var filterResponse = function(response) {
-  var ret_array = [];
-  var entities = {};
-  if (response.result !== null) {
-    $.each(response.result, function(i, result) {
-      var nameType = entityTypes[result.nametype];
-      var term = result.term;
-      if (entities[nameType] !== 'undefined') {
-       if (!entities[nameType]) {
-          entities[nameType] = [];
-        }
-        entities[nameType].push(term);
-      }
-    });
-    for (key in entities) {
-      $.each(entities[key], function(i, term) {
-        var hidden = 'hidden'
-        if (i == 0) {
-          hidden = ''
-        }
-        ret_array.push({value: term, nameType: key, hideHeader: hidden});
-      });
-    }
-  }
-  return ret_array;
-};
 
-/*suggestEngine is our Bloodhound typeahead suggestion object, which
-servers as the typeahead data source*/
-var suggestEngine = new Bloodhound({
-  name: 'identities',
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  /* limit to 10 items at a time because that's all we can get
-  from VIAF */
-  limit: 10,
-  remote: {
-    ajax: {dataType: 'jsonp'},
-    url: 'http://viaf.org/viaf/AutoSuggest?query=%QUERY',
-    filter: filterResponse
-  }
-});
+/*Templates*/
+var templates = {
+  suggestion: '<h3 class="{{hideHeader}}">{{nameType}}</h3><div class="term">{{value}}</div>',
+}
 
 
-//jquery document ready function
-$(document).ready(function() {
-  suggestEngine.initialize();
- 
-  $('#typeahead .search-input').typeahead({
-    hint: true,
-    minLength: 3
+/*View Logic*/
+
+// searchBoxView--not really a view
+var searchBoxView = {
+  selectors: {
+    searchform: '#typeahead',
+    searchbox: '#typeahead .search-input',
+    searchvalue: '#typeahead .search-input.tt-input'
   },
-  {
-    name: 'indentities',
-    displayKey: 'value',
-    source: suggestEngine.ttAdapter(),
-    templates: {
-      suggestion: Handlebars.compile('<h3 class="{{hideHeader}}">{{nameType}}</h3><div class="term">{{value}}</div>')
+
+  initialize: function() {
+    $(this.selectors.searchform).on('submit', {view: this}, this.search);
+  },
+
+  renderValue: function(query) {
+    if (query) {
+      $(this.selectors.searchbox).val(query);
     }
-  });
+  },
+
+  renderTypeahead: function() {
+    $(this.selectors.searchbox).advancedViafTypeahead({
+      templates: {
+        suggestion: Handlebars.compile(templates.suggestion)
+      }  
+    });
+  },
+
+  render: function(query) {
+    this.renderValue(query);
+    this.renderTypeahead();
+  },
+
+  search: function(ev) {
+    ev.preventDefault();
+    var view = ev.data.view;
+    var query = $(view.selectors.searchvalue).val();
+    history.pushState({}, '', '?q=' + query);
+    $(view.selectors.searchbox).typeahead('close');
+  },
+}
+
+// result view
+var resultView = {
+  selector: '#result',
+
+  render: function() {
+
+  }
+}
+
+
+/*And document ready...*/
+$(document).ready(function() {
+  /*First get the query, if there is one.*/
+  var query = getUrlParam('q');
+  searchBoxView.initialize();
+  searchBoxView.render(query);
 });
