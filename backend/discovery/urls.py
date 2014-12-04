31c5f8b1e2b8e31aa6c5922d4eb903770555b9c8
Jason Thomale <jason@Zepp.domain_not_set.invalid>
2014-12-04 11:17:04 -0500

    Working out some architectural details

--- backend/discovery/urls.py
+++ backend/discovery/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 from discovery import views
 
 urlpatterns = patterns('',
-    url(r'^bibs/', views.BibsView.as_view(), name='bib'),
+    url(r'^bibs-rdf/', views.BibsRDFView.as_view(), name='bibs-rdf'),
 )
--- backend/discovery/views.py
+++ backend/discovery/views.py
@@ -8,9 +8,22 @@ from rest_framework.response import Response
 from rest_framework import status
 import auth
 
-class BibsView(APIView):
+
+def dapi_make_request(query='', access_token=None):
+    dbIds = '638'
+    request_url = 'https://beta.worldcat.org/discovery/bib/search?' + 'q=' + query + '&' + 'dbIds=' + dbIds
+    authorization = 'Bearer ' + access_token.access_token_string
+    r = requests.get(
+        url=request_url,
+        headers={'Authorization': authorization, 'Accept': 'application/json'}
+    )
+    return r
+
+
+class BibsRDFView(APIView):
     """
-    Searches OCLC Discovery API and returns a result
+    Searches OCLC Discovery API and returns a result in json-ld
+
     """
 
     access_token = None
@@ -20,21 +33,30 @@ class BibsView(APIView):
         if self.access_token is None:
             self.access_token = auth.authenticate()
 
-        # build URL
-        query = request.query_params.get('q', 'Hamlet')
-        dbIds = '638'
-        request_url = 'https://beta.worldcat.org/discovery/bib/search?' + 'q=' + query + '&' + 'dbIds=' + dbIds
-        authorization = 'Bearer ' + self.access_token.access_token_string
-        r = requests.get(
-            url=request_url,
-            headers={'Authorization': authorization, 'Accept': 'text/turtle'}
-        )
+        # get the query and make the request
+        query = request.query_params.get('q', '')
+        r = dapi_make_request(query=query, access_token=self.access_token)
 
-        g = rdflib.Graph()
-        result = g.parse(data=r.text, format='turtle')
-        data = ''
-        for s, p, o in g:
-            data = '{}{},{},{}\n'.format(data, s, p, o)
+        return Response(r.json())
 
-        return Response(data)
 
+class BibsView(APIView):
+    access_token = None
+    """
+    Searches bibs and returns results as simple json
+        title
+        creator
+        contributors
+        subjects
+    """
+
+    def get(self, request, format=None):
+        # first authenticate and get access token if needed
+        if self.access_token is None:
+            self.access_token = auth.authenticate()
+
+        # get the query and make the request
+        query = request.query_params.get('q', '')
+        r = dapi_make_request(query=query, access_token=self.access_token)
+
+        return Response(r.json())
